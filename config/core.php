7b7782c3a0e0bedaac6faaf56d59deaa2d036762
Nenad Pesic <nenad.pesic32@gmail.com>
2020-05-19 11:13:07 +0200

    PHP CRUD with Search and Pagination

---
+++ config/core.php
@@ -0,0 +1,9 @@
+<?php
+// page given in URL parameter, default page is one
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+
+$records_per_page = 5;
+
+$from_record_num = ($records_per_page * $page) - $records_per_page;
+
+?>
---
+++ delete_product.php
@@ -0,0 +1,21 @@
+<?php
+// check if value was posted
+if($_POST){
+
+    include_once 'config/database.php';
+    include_once 'objects/product.php';
+
+    $database = new Database();
+    $db = $database->getConnection();
+
+    $product = new Product($db);
+
+    $product->id = $_POST['object_id'];
+
+    if($product->delete()){
+        echo "Object was deleted.";
+    } else {
+        echo "Unable to delete object.";
+    }
+}
+?>
